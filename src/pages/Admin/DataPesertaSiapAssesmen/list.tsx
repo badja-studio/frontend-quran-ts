f4833ba7b6baa7c91db34fd5e2361695aa096cfe
nabilhatami86 <nabilhatami27@gmail.com>
2025-12-17 11:08:03 +0700

    fix:pagination 100++

--- src/pages/Admin/DataPesertaSiapAssesmen/list.tsx
+++ src/pages/Admin/DataPesertaSiapAssesmen/list.tsx
@@ -238,7 +238,7 @@ export default function ListPagesDataPesertaSiapAssement() {
         <DataTable
           columns={columnsPeserta}
           data={transformedData}
-          rowsPerPageOptions={[5, 10, 25]}
+          rowsPerPageOptions={[5, 10, 25, 50, 100, 150]}
           emptyMessage={
             isFetching ? "Memuat data..." : "Belum ada data peserta"
           }
